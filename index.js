baf73e8afdd5bda59ec6602093748d475d7418f0
Alexander Prypyshnyi <alexpryp@gmail.com>
2025-07-23 02:45:16 +0300

    ADD | router

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import router from "./router.js";
 
 const PORT = 5000;
+const DB_URL = 'mongodb+srv://alexpryp:IzRV8rRflu81pwVc@cluster0.ymrdebj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const app = express();
 
 app.use(express.json());
+app.use('/api', router);
 
-app.post('/', (req, res) => {
-    console.log(req.query);
-    console.log(req.body);
-    res.status(200).json('Server working');
-});
+// app.post('/', );
 
-app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+}
 
+startApp();
